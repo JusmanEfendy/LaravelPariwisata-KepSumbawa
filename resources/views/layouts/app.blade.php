11ec8cf4822a313f9e94a4c8e6adacbec89c0d19
JusmanEfendy <njhussrhiee0206@gmail.com>
2022-11-27 02:16:39 -0600

    add fitur maps

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,24 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>{{ $title }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
+        integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('dist/css/maps-style/map.css') }}">
     <link href="{{ asset('dist/img/logo/logo.png') }}" rel="shortcut icon" type="image/x-icon">
     <link href="{{ asset('dist/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('dist/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('dist/css/ruang-admin.min.css') }}" rel="stylesheet">
+    <style>
+        .maps {
+            height: 100% !important;
+            cursor: pointer;
+        }
+    </style>
+    {{-- <link href="{{ asset('dist/css/map-style/leaflet-hash.js') }}" rel="stylesheet"> --}}
     {{ $head ?? '' }}
 </head>
 
@@ -70,6 +84,24 @@
     <script src="{{ asset('dist/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <script src="{{ asset('dist/js/ruang-admin.min.js') }}"></script>
     <script src="{{ asset('dist/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+    <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"
+        integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
+    {{-- <script src="{{ asset('dist/js/maps-control/map.js') }}"></script> --}}
+    {{-- <script src="{{ asset('dist/js/maps-control/leaflet-hash.js') }}"></script> --}}
+    <script>
+        const map = L.map('map').setView([-8.6327874, 117.6065781], 8);
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        map.on('click', function(e) {
+            console.log(e.latlng)
+        })
+    </script>
     {{ $script ?? '' }}
 </body>
 
