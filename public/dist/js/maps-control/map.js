a4bb08d2ef83a66b27e1b6303f7275fa8dd42948
JusmanEfendy <njhussrhiee0206@gmail.com>
2022-12-25 11:33:49 -0600

    responsive homepage

--- public/dist/js/maps-control/map.js
+++ public/dist/js/maps-control/map.js
@@ -1,5 +1,5 @@
 // MAPS
-const map = L.map('map').setView([-8.6327874, 117.6065781], 9);
+const map = L.map('map').setView([-8.6327874, 117.6065781], 8);
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,
 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
