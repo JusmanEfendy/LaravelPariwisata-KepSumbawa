4b49048f2fdf2aa68eba7c83f50c35d756254105
JusmanEfendy <njhussrhiee0206@gmail.com>
2023-10-14 15:15:55 +0700

    update METADATA SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Pariwisata Kepulauan Sumbawa">
+    <meta name="author" content="Jussman Effendy">
+    <meta name="keywords" content="pariwisata,sumbawa,kepulauan,kepulauan sumbawa,wisata sumbawa,wisata">
     <title>{{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
